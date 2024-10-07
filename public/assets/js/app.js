e27f578816ca59bb0ab4504605b5f69a3dc58550
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-10-07 20:53:11 +0500

    10.07.2024

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -99,51 +99,54 @@ modalTriggers.forEach((trigger) => {
 });
 
 // form
-[].forEach.call(document.querySelectorAll("[data-mask]"), function (input) {
-  let keyCode;
-  function mask(event) {
-    event.keyCode && (keyCode = event.keyCode);
-    let pos = this.selectionStart;
-    if (pos < 3) event.preventDefault();
-    let matrix = "+7 (___) ___-__-__",
-      i = 0,
-      def = matrix.replace(/\D/g, ""),
-      val = this.value.replace(/\D/g, ""),
-      newValue = matrix.replace(/[_\d]/g, function (a) {
-        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-      });
-    i = newValue.indexOf("_");
-    if (i != -1) {
-      i < 5 && (i = 3);
-      newValue = newValue.slice(0, i);
-    }
-    let reg = matrix
-      .substr(0, this.value.length)
-      .replace(/_+/g, function (a) {
-        return "\\d{1," + a.length + "}";
-      })
-      .replace(/[+()]/g, "\\$&");
-    reg = new RegExp("^" + reg + "$");
-    if (
-      !reg.test(this.value) ||
-      this.value.length < 5 ||
-      (keyCode > 47 && keyCode < 58)
-    )
-      this.value = newValue;
-    if (event.type == "blur" && this.value.length < 5) this.value = "";
+[].forEach.call(
+  document.querySelectorAll("input[name='phone']"),
+  function (input) {
+    let keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      let pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      let matrix = "+7 (___) ___-__-__",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        newValue = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        });
+      i = newValue.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        newValue = newValue.slice(0, i);
+      }
+      let reg = matrix
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+          return "\\d{1," + a.length + "}";
+        })
+        .replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (
+        !reg.test(this.value) ||
+        this.value.length < 5 ||
+        (keyCode > 47 && keyCode < 58)
+      )
+        this.value = newValue;
+      if (event.type == "blur" && this.value.length < 5) this.value = "";
 
-    if (this.value.length == 18 || this.value.length == 0) {
-      input.dataset.numbervalid = "true";
-    } else {
-      input.dataset.numbervalid = "false";
+      if (this.value.length == 18 || this.value.length == 0) {
+        input.dataset.numbervalid = "true";
+      } else {
+        input.dataset.numbervalid = "false";
+      }
     }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
   }
-
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-  input.addEventListener("blur", mask, false);
-  input.addEventListener("keydown", mask, false);
-});
+);
 
 const forms = document.querySelectorAll("form");
 forms.forEach((form) => {
@@ -196,6 +199,29 @@ var introSwiper = new Swiper(".intro .swiper", {
   pagination: {
     el: ".intro .swiper-pagination",
     clickable: true,
+  },
+});
+
+var gallerySwiper = new Swiper(".gallery .swiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".gallery .swiper-button-next",
+    prevEl: ".gallery .swiper-button-prev",
+  },
+  breakpoints: {
+    // 641:{
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    // },
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1025: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
 });
 
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,4 +1,5 @@
 import TheFooter from '../components/TheFooter.astro';
+import TheForm from '../components/TheForm.astro';
 import TheHeader from '../components/TheHeader.astro';
 import TheModal from '../components/TheModal.astro';
-export { TheFooter, TheHeader, TheModal };
+export { TheFooter, TheForm, TheHeader, TheModal };
